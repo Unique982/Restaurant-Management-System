d12cfe0204b3b17253e3ccbb7022dc9144202ce2
Unique Neupane <khemrajneupane111@gmail.com>
2025-09-01 13:50:54 +0545

    this code only testing purpose addded

--- server/src/controller/globals/auth/auth.Controller.ts
+++ server/src/controller/globals/auth/auth.Controller.ts
@@ -1,4 +1,12 @@
 // login
+import { Request, Response } from "express";
+class LoginUser {
+  static createUser = async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+    // this code only testing purpose
+  };
+}
+
 // register
 // forget password
 // otp vrification
