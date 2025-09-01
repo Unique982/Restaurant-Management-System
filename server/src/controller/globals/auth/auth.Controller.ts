8b5b7a5ff568df4a2d6d39122831ec525ab84591
Unique Neupane <khemrajneupane111@gmail.com>
2025-09-01 13:56:49 +0545

    upload code register branch 2

--- server/src/controller/globals/auth/auth.Controller.ts
+++ server/src/controller/globals/auth/auth.Controller.ts
@@ -5,6 +5,12 @@ class LoginUser {
     const { email, password } = req.body;
     // this code only testing purpose
   };
+
+  // registe r
+  static userRegsiter = async (req: Request, res: Response) => {
+    const { username, email, password, role } = req.body;
+    // testing new branch
+  };
 }
 
 // register
