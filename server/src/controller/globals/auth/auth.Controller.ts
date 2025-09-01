be488f6622acf2861ba01565476af649b9d4010c
Unique Neupane <khemrajneupane111@gmail.com>
2025-09-01 14:34:33 +0545

    Remove createLogin function from develop

--- server/src/controller/globals/auth/auth.Controller.ts
+++ server/src/controller/globals/auth/auth.Controller.ts
@@ -1,11 +1,6 @@
 // login
 import { Request, Response } from "express";
 class LoginUser {
-  static createUser = async (req: Request, res: Response) => {
-    const { email, password } = req.body;
-    // this code only testing purpose
-  };
-
   // registe r
   static userRegsiter = async (req: Request, res: Response) => {
     const { username, email, password, role } = req.body;
