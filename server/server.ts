47618c3e901fb7cf39be6555be138cae83c2ce7f
Niraj3004 <nirajkushwaha84@gmail.com>
2025-09-02 21:57:22 +0545

    database connection setup

--- server/server.ts
+++ server/server.ts
@@ -1,14 +1,17 @@
 import app from "./src/app";
-
-//dotenv config garna
 import { config } from "dotenv";
+config()
 
-config();
-// create server
-const startServer = () => {
-  const port = process.env.PORT || 3000;
-  app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
-  });
-};
-startServer();
+//database connection import 
+// import garena vane connect hudainw or file execute hudainw 
+import "./src/database/connection"
+function startServer(){
+    const port = process.env.PORT 
+    app.listen(port,function(){
+        console.log(`Server has started at port ${port}`)
+    }) 
+}
+
+startServer()
+
+//sudo /Applications/XAMPP/xamppfiles/xampp start
