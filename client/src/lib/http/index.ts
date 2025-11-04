6156ba407ab1bac228300b036def35e0486d9382
Unique Neupane <khemrajneupane111@gmail.com>
2025-11-04 17:06:18 +0545

    Change API base URL to production
    Updated API base URL to production server.

--- client/src/lib/http/index.ts
+++ client/src/lib/http/index.ts
@@ -1,10 +1,12 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:4000/api/",
+  baseURL: "https://restaurant-management-system-j9p7.vercel.app/ ",
+  //https://restaurant-management-system-j9p7.vercel.app/ http://localhost:4000/api/
   headers: {
     "Content-Type": "application/json", // send vayirako data ko formate
     Accept: "application/json", // reciver huda kasto type ko fomate ko receive garna
   },
 });
 export default API;
+
