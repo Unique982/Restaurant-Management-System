5ea3f231d246bda8b100257365008b7fb32a358b
Unique Neupane <khemrajneupane111@gmail.com>
2025-11-29 10:56:42 +0545

    http chnage

--- client/next.config.ts
+++ client/next.config.ts
@@ -24,6 +24,10 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
+        hostname: "images.pexels.com",
+      },
+      {
+        protocol: "https",
         hostname: "res.cloudinary.com",
       },
       { protocol: "https", hostname: "cdn-icons-png.flaticon.com" },
